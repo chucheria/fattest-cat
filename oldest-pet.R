3bf18f2745b8813856a8bac0a472e135756cba25
Bea <bea.hernandez@datmean.com>
2017-03-19 21:15:33 +0100

    added yayos to dogs

--- oldest-pet.R
+++ oldest-pet.R
@@ -7,24 +7,33 @@ library(httr)
 ANAA_BASE <- "http://www.anaaweb.org/"
 
 choice <- readline(prompt="Elige entre: \n 1 = El gato más mayor \n 2 = El perro más mayor \n");
-choice <- if (choice == 1) 'gatos' else 'perros'
+choice <- ifelse(choice == 1, 'gatos', 'perros')
 
-fetch_helper <- function(choice, page = 0) {
-  message("Leyendo la página de " , choice)
+fetch_helper <- function(pet, page = 0) {
+  message("Leyendo la página de " , pet)
   page_query <- if (page < 1) NULL else list(page = page)
-  modifier <- paste0("adopciones-de-", choice)
+  modifier <- paste0("adopciones-de-", pet)
   url <- modify_url(ANAA_BASE, path = c("adopciones", modifier),
                     query = page_query)
   ids <- url %>%
     read_html() %>%
     html_nodes("a") %>%
-    html_attr("href") %>%
-    str_subset(paste0("/", modifier, "/"))
+    html_attr("href")
+  if (pet == 'y-ayos') {
+    ids <- ids %>%
+      str_subset("/disfruta-de-un-y-ayo-en-casa/adopciones-de-perros/")
+  } else {
+    ids <- ids %>%
+      str_subset(paste0("/", modifier, "/"))
+  }
   ids <- sapply(strsplit(as.character(ids), "/"), tail, 1)
 }
 
 message("Accediendo a ANAA (Departamento de ", choice, "...)")
 ids <- fetch_helper(choice) %>% unique()
+if (choice == 'perros') {
+  ids_yayos <- fetch_helper(pet = 'y-ayos') %>% unique()
+}
 message("Leyendo información sobre ", choice, ".\n  ",
         length(ids), " ", choice, " únicos encontrados.\n ",
         "Calculando su edad...")
@@ -34,12 +43,30 @@ calls <-
     id = ids,
     url = map_chr(id,
                   ~ modify_url(ANAA_BASE,
-                               path = c("adopciones", paste0("adopciones-de-", choice), .x))),
-    resp = map(url, GET),
-    status = map_chr(resp, status_code)
-  ) %>%
-  filter(status == "200") %>%
-  mutate(content = map(resp, content))
+                               path = c("adopciones", paste0("adopciones-de-", choice), .x)))
+  )
+
+if (choice == 'perros') {
+  calls_yayos <-
+    tibble(
+      id = ids_yayos,
+      url = map_chr(id,
+                    ~ modify_url(ANAA_BASE,
+                                 path = c("disfruta-de-un-y-ayo-en-casa", paste0("adopciones-de-", choice), .x)))
+    )
+
+  calls <- bind_rows(calls, calls_yayos) %>%
+    mutate(resp = map(url, GET),
+           status = map_chr(resp, status_code)) %>%
+    filter(status == "200") %>%
+    mutate(content = map(resp, content))
+} else {
+  calls <- calls %>%
+    mutate(resp = map(url, GET),
+           status = map_chr(resp, status_code)) %>%
+    filter(status == "200") %>%
+    mutate(content = map(resp, content))
+}
 
 get_field <- compose(str_trim, html_text, html_node)
 pet <- calls %>%
@@ -50,13 +77,13 @@ pet <- calls %>%
   select(name, age, sex, url, id) %>%
   mutate(born = as.Date(gsub("Edad: ", "", age), '%d-%m-%Y'),
          sex = gsub("Sexo: ", "", sex),
-         pronoun = ifelse(sex == "hembra", "Ella", "Él"),
+         article = ifelse(sex == "hembra", "La", "El"),
          age = round(as.numeric(difftime(Sys.Date(), born, units = "weeks"))/52.25, digits=1))
 
 tbl <-
   capture.output(
     pet %>%
-      select(name, age, sex, id) %>%
+      select(name, age, sex) %>%
       as.data.frame() %>%
       print()
   ) %>%
@@ -65,7 +92,7 @@ message(tbl)
 
 oldest <- min(pet$born)
 message(
-  str_interp("El más mayor es ${name}. ${pronoun} tiene ${age} años.",
+  str_interp("${article} más mayor es ${name}. Tiene ${age} años.",
              pet[pet$born == oldest, ])
 )
 Sys.sleep(0.5)
